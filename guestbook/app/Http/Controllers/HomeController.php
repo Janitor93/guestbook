8044ccbb1f831afcad818a4729aec651aea586dd
Ruslan <ruslan.penkovskij@gmail.com>
2016-03-31 14:07:17 +0300

    almost complete

--- guestbook/app/Http/Controllers/HomeController.php
+++ guestbook/app/Http/Controllers/HomeController.php
@@ -5,13 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Models\Message;
 
 class HomeController extends Controller
 {
     public function index() {
       $data = [
         'title' => 'Guest Book on Laravel 5',
-        'pagetitle' => "Guset Book"
+        'pagetitle' => "Guset Book",
+        'messages' => Message::latest()->paginate(1),
+        'count' => Message::count()
       ];
       return view('pages.messages.index', $data);
     }
